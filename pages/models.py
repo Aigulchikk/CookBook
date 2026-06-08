4b12717b92896566fa93ffa10da611044d10a4b3
Aigulchikk <ajgulusipova80@gmail.com>
2026-06-08 11:16:57 +0300

    Task 5 ready: PostgreSQL, .env, Recipe model, admin

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    ingredients = models.TextField(verbose_name="Ингредиенты")
+    instructions = models.TextField(verbose_name="Инструкция приготовления")
+    cooking_time = models.IntegerField(verbose_name="Время приготовления (мин)", default=30)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Рецепт"
+        verbose_name_plural = "Рецепты"
